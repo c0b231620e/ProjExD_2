dbfda7f1af52ecf970655ede2a4ba3d21ba6f08b
yutaka <c0b231620e@edu.teu.ac.jp>
2024-06-18 14:04:08 +0900

    練１移動の辞書化

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,6 +4,12 @@ import pygame as pg
 
 
 WIDTH, HEIGHT = 1600, 900
+DELTA={  #移動用辞書        
+       pg.K_UP:(0,-5),
+       pg.K_DOWN:(0,5),
+       pg.K_LEFT:(-5,0),
+       pg.K_RIGHT:(5,0)
+}
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -12,27 +18,23 @@ def main():
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
-    kk_rct = kk_img.get_rect()
+    kk_rct = kk_img.get_rect()  # 座標系
     kk_rct.center = 900, 400
     clock = pg.time.Clock()
     tmr = 0
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: 
+            if event.type == pg.QUIT:  # 罰を押したら終了するように設定
                 return
         screen.blit(bg_img, [0, 0]) 
 
-        key_lst = pg.key.get_pressed()
-        sum_mv = [0, 0]
-        if key_lst[pg.K_UP]:
-            sum_mv[1] -= 5
-        if key_lst[pg.K_DOWN]:
-            sum_mv[1] += 5
-        if key_lst[pg.K_LEFT]:
-            sum_mv[0] -= 5
-        if key_lst[pg.K_RIGHT]:
-            sum_mv[0] += 5
-        kk_rct.move_ip(sum_mv)
+        key_lst = pg.key.get_pressed()  # 押したキーを取得
+        sum_mv = [0, 0]# 移動用のリスト
+        for k,v in DELTA.items():
+            if key_lst[k]:
+                sum_mv[0] += v[0]
+                sum_mv[1] += v[1]
+        kk_rct.move_ip(sum_mv)# 移動させる
         screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1
